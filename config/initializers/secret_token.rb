fd524296d80d860da1b0ecde97b33dc4a4bf2ac7
D Tucker <danielle@tuckerlabs.com>
2013-07-23 08:37:07 -0700

    Initial Rails 3 setup with rspec and gem dependencies

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+KnowledgeBase::Application.config.secret_token = '27017e4e6cac359e2e473586696c3a95edaee1f778d509d56da330c30f73c3e7ea2d6160981e7a487fa4f0747f2603f4204f31547ddfd8f53d79bfcf7f724d59'
