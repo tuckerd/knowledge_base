b920fcf6e1c786ec5ce251e1922d6a35bb9c9e9d
D Tucker <danielle@tuckerlabs.com>
2013-07-23 09:30:21 -0700

    Creation of question test passing

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -7,4 +7,19 @@ class QuestionsController < ApplicationController
   def new
     @question = Question.new
   end
+
+  def create
+    @question = Question.new(params[:question])
+    if @question.save
+      flash[:notice] = "Question has been created."
+      redirect_to @question
+    else
+      #not yet
+    end
+  end
+
+  def show
+    @question = Question.find(params[:id])
+  end
+
 end
--- db/migrate/20130723155837_create_questions.rb
+++ db/migrate/20130723155837_create_questions.rb
@@ -2,7 +2,7 @@ class CreateQuestions < ActiveRecord::Migration
   def change
     create_table :questions do |t|
       t.string :title
-      t.string :answer
+      t.text :answer
 
       t.timestamps
     end
