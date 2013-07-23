f6f3ddf5f7c56b2917695956311c9287089c7f49
D Tucker <danielle@tuckerlabs.com>
2013-07-23 10:16:32 -0700

    Add title on page dynamically generated

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def title(*words)
+    unless words.empty?
+      content_for :title do
+        (words << "Knowledge Base").join(" - ")
+      end
+    end
+  end
 end
--- spec/integration/creating_questions_spec.rb
+++ spec/integration/creating_questions_spec.rb
@@ -8,4 +8,17 @@ feature 'Creating Questions' do
     click_button 'Create Question'
     page.should have_content('Question has been created.')
   end
+
+  scenario 'page title is based on Question Name' do
+    visit '/'
+    click_link 'New Question'
+    fill_in 'I want to know...', with: "How old is the Universe?"
+    click_button 'Create Question'
+    page.should have_content('Question has been created.')
+
+    question = Question.find_by_title("How old is the Universe?")
+    page.current_url.should == question_url(question)
+    title = "How old is the Universe? - Question - Knowledge Base"
+    find('title').should have_content(title)
+  end    
 end
